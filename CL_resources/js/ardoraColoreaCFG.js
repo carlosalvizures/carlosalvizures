bb52b60dc2974b7b4b798626293cf80ea894d23d
carlosalvizures <1galvizuresdcarlosadrian@gmail.com>
2022-03-21 10:47:02 -0600

    Colorear
    Colorea al Robot

---
+++ CL_resources/js/ardoraColoreaCFG.js
@@ -0,0 +1,36 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=0; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Inicia";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Lo hiciste muy bien"; messageTime="Te quedaste sin tiempo"; messageError="Te equivocaste, vuelve a intentarlo!!"; messageErrorG="Te equivocaste, vuelve a intentarlo!!"; messageAttempts="No tienes más intentos"; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="Q0w="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var joinPar=[];
+var c=["#FFFF00","#FFFF00","#00FFFF","#008000","#400080","#FF80FF","#FF80FF","#FF8000","#FF0000","#FF0000"];
+var x=["294","162","242","239","256","169","338","235","414","62"];
+var y=["173","161","105","217","285","351","345","54","201","172"];
+var t=["1","1","2","3","4","5","5","6","7","7"];
+var cM=["#FFFF00","#00FFFF","#008000","#400080","#FF80FF","#FF8000","#FF0000"];
+var tM=["1","2","3","4","5","6","7"];
+var indexColor=0;
