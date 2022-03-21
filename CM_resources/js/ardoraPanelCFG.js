212b5bfc3c68bc99acb424cc279f08261aa91861
carlosalvizures <1galvizuresdcarlosadrian@gmail.com>
2022-03-21 12:03:10 -0600

    Continentes
    Continentes del mundo

---
+++ CM_resources/js/ardoraPanelCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=6; attempts=0; attemptsMax=1;
+var score=0; scoreMax=6; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Bien hecho"; messageTime="Sin tiempo"; messageError="Te equivocaste"; messageAttempts="Ya no tienes intentos"; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var texSel=0; var doneA=["0","0","0","0","0","0"];
+var posX=[-5,155,203,256,165,-2]; var posY=[11,5,44,115,87,123];
+var coorx=["NjU=", "MTcw", "MjI3", "MjUx", "MTYw", "OTE="]; var coory=["NDQ=", "MzQ=", "NTI=", "MTE1", "ODc=", "MTA1"]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var al1=["Sur América", "Asia", "Norte América", "África", "Norte América", "Europa"]; var al2=["Europa", "Centroamerica", "África", "Centroamerica", "El Caribe", "Asia"]; var al3=["Asia", "Oceania", "Europa", "Sur América", "Asia", "África"]; var al4=["", "", "", "", "", ""]; answers=["Norte América", "Europa", "Asia", "Oceania", "África", "Sur América"];indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="Q00="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
